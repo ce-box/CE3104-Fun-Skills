aed61aab0abf770e39b84146897ffd766c161b6c
Esteban Alvarado <estalvgs1999@gmail.com>
2019-11-02 03:05:59 -0600

    [doc] documented init class

--- Compiler/src/ide/init.py
+++ Compiler/src/ide/init.py
@@ -1,3 +1,17 @@
+# ------------------------------------------------------------
+# File: init.py
+# Developed by: Esteban Alvarado Vargas
+# Project: FunSkills-[Compiler]
+# version: 3.0
+# last edited by: Esteban Alvarado:: 1/11/19 03.30
+#
+# Description: Initial window with an animation of loading
+#              progress, is the previous one of the program in
+#              the style of the editors of jetbrains.
+#
+# TEC 2019 | CE3104 - Lenguajes, Compiladores e Interpretes
+# ------------------------------------------------------------
+
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -5,6 +19,10 @@ from PyQt5.QtWidgets import *
 import time
 import threading
 import src.ide.IDE as ide
+
+# Path variables
+directory_path = "/home/esteban/Documentos/TEC/2S 2019/Lenguajes-Compiladores-Intérpretes/2. Compiladores/2. Proyecto/CE3104-Fun-Skills/Compiler/"
+img_path = directory_path + "src/img/intro.png"
 
 TIME_LIMIT = 100
 BAR_STYLE = """QProgressBar{
@@ -17,12 +35,10 @@ QProgressBar::chunk{
     background: #ffffff
 }"""
 
-
-directory_path = "/home/esteban/Documentos/TEC/2S 2019/Lenguajes-Compiladores-Intérpretes/2. Compiladores/2. Proyecto/CE3104-Fun-Skills/Compiler/"
-img_path = directory_path + "src/img/intro.png"
-
+# Init window class
 class init_window(QWidget):
 
+    # Constructor
     def __init__(self, parent=None):
         super().__init__(parent)
         self.width = 640
@@ -47,6 +63,7 @@ class init_window(QWidget):
         self.progress.setStyleSheet(BAR_STYLE)
 
 
+# @brief - progress counter of the progress bar, runs in a separate thread
 def count(app,window):
     count = 0
     while count < TIME_LIMIT:
@@ -56,6 +73,8 @@ def count(app,window):
     time.sleep(1)
     app.quit()
 
+# @brief - main function that runs the animation and launches
+#          the code editor
 def init_IDE():
     app = QApplication(sys.argv)
     window = init_window()
@@ -64,6 +83,7 @@ def init_IDE():
     x = threading.Thread(target=count,args=(app,window,))
     x.start()
     app.exec_()
+
 
 if __name__ == '__main__':
     init_IDE()
